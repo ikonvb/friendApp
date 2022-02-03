d85dc7eb2e2b1bd2e5d29bc22cb8ba08c12c08b6
kostyaFrom <korustlt@gmail.com>
2022-02-03 16:33:46 +0300

    implemented post mapping method

--- src/main/java/com/bulyginkonstantin/friend_app/controllers/RegistrationController.java
+++ src/main/java/com/bulyginkonstantin/friend_app/controllers/RegistrationController.java
@@ -5,8 +5,13 @@ import com.bulyginkonstantin.friend_app.service.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/register")
@@ -20,6 +25,22 @@ public class RegistrationController {
     @GetMapping
     public String register(Client client) {
         return "register";
+    }
+
+    @PostMapping
+    public String register(@Valid Client client, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
+        if (!client.getPassword().equalsIgnoreCase(client.getConfirmPassword())) {
+            model.addAttribute("passNotMatch", "Password do not match");
+            return "register";
+        }
+
+        client.setPassword(passwordEncoder.encode(client.getPassword()));
+        clientRepository.save(client);
+        return "redirect:/login";
     }
 
 }
