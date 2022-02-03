c114e33220aba23fa2b928038a74ae9c32308c1d
kostyaFrom <korustlt@gmail.com>
2022-02-03 15:01:59 +0300

    implemented Client entity

---
+++ src/main/java/com/bulyginkonstantin/friend_app/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.bulyginkonstantin.friend_app.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+}
