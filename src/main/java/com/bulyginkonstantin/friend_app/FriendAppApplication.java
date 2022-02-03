0359ae1c7ef1ce3985362e4bcaa044a1ec7da201
kostyaFrom <korustlt@gmail.com>
2022-02-03 13:36:29 +0300

    init commit

---
+++ src/main/java/com/bulyginkonstantin/friend_app/FriendAppApplication.java
@@ -0,0 +1,13 @@
+package com.bulyginkonstantin.friend_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bulyginkonstantin/friend_app/FriendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulyginkonstantin.friend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
