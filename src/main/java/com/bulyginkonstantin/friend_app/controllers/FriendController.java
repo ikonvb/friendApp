9c59d964108cbbd111c0075c5e61d3711054910f
kostyaFrom <korustlt@gmail.com>
2022-02-07 16:14:03 +0300

    added embeddable key

--- src/main/java/com/bulyginkonstantin/friend_app/controllers/FriendController.java
+++ src/main/java/com/bulyginkonstantin/friend_app/controllers/FriendController.java
@@ -1,7 +1,14 @@
 package com.bulyginkonstantin.friend_app.controllers;
 
+import com.bulyginkonstantin.friend_app.data.Client;
+import com.bulyginkonstantin.friend_app.data.ClientFriendKey;
+import com.bulyginkonstantin.friend_app.data.Friend;
+import com.bulyginkonstantin.friend_app.repository.FriendsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,10 +17,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FriendController {
 
 
-    @GetMapping("/addToFriend/{id}")
-    public String addFriend(@RequestParam("id") int id) {
-        return "friends";
+    @Autowired
+    FriendsRepository friendsRepository;
+
+    @GetMapping("/addToFriend/{currentId}/{id}")
+    public String addFriend(@PathVariable int currentId, @PathVariable int id, Model model) {
+        Friend friend = new Friend();
+        ClientFriendKey key = new ClientFriendKey(currentId, id);
+        friend.setId(key);
+        friendsRepository.save(friend);
+        Client client = new Client();
+        model.addAttribute("client", client);
+        model.addAttribute("currentClientId", currentId);
+        return "findfriend";
     }
+
 
     @GetMapping("/remove/{id}")
     public String remove(@RequestParam("id") int id) {
---
+++ src/main/java/com/bulyginkonstantin/friend_app/data/ClientFriendKey.java
@@ -0,0 +1,49 @@
+package com.bulyginkonstantin.friend_app.data;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class ClientFriendKey implements Serializable {
+
+    private int clientId;
+    private int friendId;
+
+    public ClientFriendKey(int clientId, int friendId) {
+        this.clientId = clientId;
+        this.friendId = friendId;
+    }
+
+    public ClientFriendKey() {
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(int friendId) {
+        this.friendId = friendId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClientFriendKey that = (ClientFriendKey) o;
+        return clientId == that.clientId && friendId == that.friendId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clientId, friendId);
+    }
+}
