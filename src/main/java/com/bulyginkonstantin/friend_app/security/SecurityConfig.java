1f22f8cdefd0f2190088ea4b2e88d6d980444949
Konstantin Bulygin <korustlt@gmail.com>
2022-02-10 15:05:00 +0300

    Update SecurityConfig.java

--- src/main/java/com/bulyginkonstantin/friend_app/security/SecurityConfig.java
+++ src/main/java/com/bulyginkonstantin/friend_app/security/SecurityConfig.java
@@ -10,6 +10,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll();
+                .antMatchers("/").permitAll()
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/");
     }
 }
